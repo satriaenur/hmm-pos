b04beb04e241fa1bfcfdbfaf6fe08b8c6ba3eb2b
a_braincode <satrianur@outlook.com>
2017-02-04 06:36:36 +0700

    add file train and test

--- hmm.py
+++ hmm.py
@@ -1,2 +1,16 @@
-def trainhmm(file):
+def learnparam(filetrain):
+	emit,transition,context = [],[],[]
+	status = False
+	f = open(filetrain,'r')
+	for line in f:
+		if line==', ,\n':
+			continue
+		elif line=='\n':
+			status = True
+			continue
+		if status:
+			print line
+			status = False
+
+learnparam('pos.train.txt')
 	
